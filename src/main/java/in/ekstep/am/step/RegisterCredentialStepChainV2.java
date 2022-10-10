6ea5f8f5cf45ca13b835b1b2bd2aa75d25bdb349
anildesai61 <75323039+anildesai61@users.noreply.github.com>
2022-10-10 18:20:21 +0530

    Add files via upload

---
+++ src/main/java/in/ekstep/am/step/RegisterCredentialStepChainV2.java
@@ -0,0 +1,46 @@
+package in.ekstep.am.step;
+
+import in.ekstep.am.builder.RegisterCredentialResponseBuilder;
+import in.ekstep.am.dto.credential.RegisterCredentialRequest;
+import in.ekstep.am.external.AmAdminApi;
+import in.ekstep.am.jwt.Base64Util;
+import in.ekstep.am.jwt.KeyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+
+@Component
+public class RegisterCredentialStepChainV2 {
+
+  @Autowired
+  private KeyManager keyManager;
+
+  public void execute(String userName, RegisterCredentialRequest request, RegisterCredentialResponseBuilder responseBuilder) throws Exception {
+    for (Step step : stepChain(userName, request, responseBuilder)) {
+      if (responseBuilder.successful()) {
+        step.execute();
+      }
+    }
+  }
+
+  private List<Step> stepChain(String userName, RegisterCredentialRequest request, RegisterCredentialResponseBuilder responseBuilder) {
+
+    SignCredentialWithKeyStep createCredentialWithKeyStep = new SignCredentialWithKeyStep(
+            userName, responseBuilder, request.key(), keyManager);
+    return asList(createCredentialWithKeyStep);
+  }
+}
---
+++ src/main/java/in/ekstep/am/step/SignCredentialWithKeyStep.java
@@ -0,0 +1,42 @@
+package in.ekstep.am.step;
+
+import in.ekstep.am.builder.CredentialDetails;
+import in.ekstep.am.jwt.JWTUtil;
+import in.ekstep.am.jwt.KeyData;
+import in.ekstep.am.jwt.KeyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.PrivateKey;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignCredentialWithKeyStep implements Step {
+
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
+  private String userName;
+  private CredentialDetails responseBuilder;
+  private String key;
+  private KeyManager keyManager;
+
+  SignCredentialWithKeyStep(String userName, CredentialDetails responseBuilder, String key, KeyManager keyManager) {
+    this.userName = userName;
+    this.responseBuilder = responseBuilder;
+    this.key = key;
+    this.keyManager = keyManager;
+  }
+
+  @Override
+  public void execute() throws Exception {
+     responseBuilder.setKey(key);
+     KeyData keyData = keyManager.getRandomKey();
+     String issuerStr = keyData.getKeyId() + "-" + key + "-" + System.currentTimeMillis();
+     responseBuilder.setToken(JWTUtil.createRS256Token(issuerStr, keyData.getPrivateKey(), createHeaderOptions(keyData.getKeyId())));
+  }
+
+  private Map<String, String> createHeaderOptions(String keyId) {
+    Map<String, String> headers = new HashMap<>();
+    headers.put("kid", keyId);
+    return headers;
+  }
+}
